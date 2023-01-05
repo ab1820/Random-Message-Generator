ca997871c982431008f53b81760f704956ffe610
Amanda Buttineau <114027684+ab1820@users.noreply.github.com>
2023-01-05 18:20:53 -0500

    adding author in JS from API

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const quoteText = document.querySelector("quote");
-quoteBtn = document.querySelector("button");
+const quoteText = document.querySelector(".quote");
+const authorName = document.querySelector(".author .name");
+const quoteBtn = document.querySelector("button");
 
 function randomQuote() {
-  console.log("clicked")
+  // fetching random quotes from API and parsing into JS object
+  fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+    console.log(result);
+    quoteText.innerText = result.content; //acessing content from API objects
+  });
 }
 
 quoteBtn.addEventListener('click', randomQuote);
